db6bd6110e48403ad686753cd68de82eae90ab31
CaptureB <airdavid62@gmail.com>
2023-02-17 11:10:20 -0700

    Updated seeds file with employees & roles

---
+++ DB/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department(name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+INSERT INTO role (title,salary,department_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Lead Engineer", 150000, 2),
+        ("Accountant", 90000, 3),
+        ("Lawyer", 160000, 4);
+
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES  ("Kobe","Bryant",1,)
+        ("LeBron","James",2)
+        ("Steve","Jobs",3)
+        ("Magic","Johnson",4);    
